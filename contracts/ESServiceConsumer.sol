3d89eb55166482549441163fc616ab42c6177a76
Shay Finkelstein <shay.finkelstein@gmail.com>
2018-06-07 17:43:35 +0200

    check transferability

--- contracts/ESServiceConsumer.sol
+++ contracts/ESServiceConsumer.sol
@@ -7,20 +7,20 @@ contract ESServiceConsumer is DSServiceConsumerInterface, EternalStorageClient {
   constructor(address _address, string _namespace) public EternalStorageClient(_address, _namespace) {}
 
   modifier onlyMaster {
-    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getAddress8("services",  TRUST_SERVICE));
+    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getDSService(TRUST_SERVICE));
     require(this.owner() == msg.sender || trustManager.getRole(msg.sender) == trustManager.MASTER());
     _;
   }
 
   modifier onlyIssuerOrAbove {
-    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getAddress8("services", TRUST_SERVICE));
+    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getDSService(TRUST_SERVICE));
     require(trustManager.getRole(msg.sender) == trustManager.ISSUER()
     || trustManager.getRole(msg.sender) == trustManager.MASTER());
     _;
   }
 
   modifier onlyExchangeOrAbove {
-    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getAddress8("services", TRUST_SERVICE));
+    DSTrustServiceInterface trustManager = DSTrustServiceInterface(getDSService(TRUST_SERVICE));
     require(trustManager.getRole(msg.sender) == trustManager.EXCHANGE()
     || trustManager.getRole(msg.sender) == trustManager.ISSUER()
     || trustManager.getRole(msg.sender) == trustManager.MASTER());
--- contracts/token/DSToken.sol
+++ contracts/token/DSToken.sol
@@ -4,6 +4,7 @@ import "./ESStandardToken.sol";
 import "./ESPausableToken.sol";
 import "../zeppelin/token/ERC20/DetailedERC20.sol";
 import "../ESServiceConsumer.sol";
+import "../compliance/DSComplianceServiceInterface.sol";
 
 contract DSToken is ESServiceConsumer,ESStandardToken,ESPausableToken,DetailedERC20 {
 
@@ -18,6 +19,37 @@ contract DSToken is ESServiceConsumer,ESStandardToken,ESPausableToken,DetailedER
     ESServiceConsumer(_storageAddress, _namespace)
     public
     {}
+
+
+
+
+    /**
+    * @dev Checks whether it can transfer or otherwise throws.
+    */
+    modifier canTransfer(address _sender, address _receiver, uint256 _value) {
+        DSComplianceServiceInterface complianceManager = DSComplianceServiceInterface(getDSService(COMPLIANCE_SERVICE));
+        complianceManager.validate(_sender,_receiver,_value);
+        _;
+    }
+
+    /**
+     * @dev override for transfer with modifier
+     * @param _to The address that will receive the tokens.
+     * @param _value The amount of tokens to be transferred.
+     */
+    function transfer(address _to, uint256 _value) canTransfer(msg.sender, _to, _value) public returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    /**
+    * @dev override for transferFrom with modifier
+    * @param _from The address that will send the tokens.
+    * @param _to The address that will receive the tokens.
+    * @param _value The amount of tokens to be transferred.
+    */
+    function transferFrom(address _from, address _to, uint256 _value) canTransfer(_from, _to, _value) public returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
 
 
 
