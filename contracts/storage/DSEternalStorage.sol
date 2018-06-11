7458ee5d28ce087acb649d4efe853a976b4f1f50
Adam Zigdon <adam@securitize.io>
2018-06-11 10:21:34 +0300

    wip registry service

--- contracts/storage/DSEternalStorage.sol
+++ contracts/storage/DSEternalStorage.sol
@@ -3,17 +3,6 @@ pragma solidity ^0.4.21;
 import "../zeppelin/storage/EternalStorage.sol";
 
 contract DSEternalStorage is EternalStorage {
-  // struct Registry {
-  //   mapping(bytes32 => bytes[]) _bool;
-  //   mapping(bytes32 => bytes[]) _int;
-  //   mapping(bytes32 => bytes[]) _uint;
-  //   mapping(bytes32 => bytes[]) _string;
-  //   mapping(bytes32 => bytes[]) _address;
-  //   mapping(bytes32 => bytes[]) _bytes;
-  // }
-
-  // Registry internal r;
-
   function deleteBoolean(bytes32 h) public onlyRole("write") {
     delete(s._bool[h]);
   }
@@ -37,76 +26,4 @@ contract DSEternalStorage is EternalStorage {
   function deleteBytes(bytes32 h) public onlyRole("write") {
     delete(s._bytes[h]);
   }
-
-  // function registerBoolean(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._bool[h].add(v);
-  // }
-
-  // function registerInt(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._int[h].add(v);
-  // }
-
-  // function registerUint(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._uint[h].add(v);
-  // }
-
-  // function registerAddress(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._address[h].add(v);
-  // }
-
-  // function registerString(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._string[h].add(v);
-  // }
-
-  // function registerBytes(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._bytes[h].add(v);
-  // }
-
-  // function registerBoolean(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._bool[h].add(v);
-  // }
-
-  // function registerInt(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._int[h].add(v);
-  // }
-
-  // function registerUint(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._uint[h].add(v);
-  // }
-
-  // function registerAddress(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._address[h].add(v);
-  // }
-
-  // function registerString(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._string[h].add(v);
-  // }
-
-  // function registerBytes(bytes32 h, bytes32 v) public onlyRole("write") {
-  //   r._bytes[h].add(v);
-  // }
-
-  // function deleteBooleanFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._bool[h]);
-  // }
-
-  // function deleteIntFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._int[h]);
-  // }
-
-  // function deleteUintFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._uint[h]);
-  // }
-
-  // function deleteAddressFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._address[h]);
-  // }
-
-  // function deleteStringFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._string[h]);
-  // }
-
-  // function deleteBytesFromRegistry(bytes32 h) public onlyRole("write") {
-  //   delete(r._bytes[h]);
-  // }
 }
