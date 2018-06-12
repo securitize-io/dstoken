0fce4fc3fc70e4ba09820a3cd33b0e1d8179eae8
adam.zigdon@gmail.com <adam@securitize.io>
2018-06-12 19:03:55 +0300

    add some documentation

--- contracts/trust/DSTrustServiceInterface.sol
+++ contracts/trust/DSTrustServiceInterface.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.23;
 
+/**
+ * @title DSTrustServiceInterface
+ * @dev An interface for a trust service which allows role-based access control for other contracts
+ */
 contract DSTrustServiceInterface {
   event DSTrustServiceRoleAdded(address _address, uint8 _role, address _sender);
   event DSTrustServiceRoleRemoved(address _address, uint8 _role, address _sender);
@@ -19,9 +23,34 @@ contract DSTrustServiceInterface {
     _;
   }
 
+  /**
+   * @dev Sets or removes a role for a wallet. (internal)
+   * @param _address The wallet whose role needs to be set or removed.
+   * @param _role The role to be set. NONE (0) indicates role removal.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setRoleImpl(address _address, uint8 _role) internal returns (bool);
+  /**
+   * @dev Transfers the ownership (MASTER role) of the contract.
+   * @param _address The address which the ownership needs to be transferred to.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setOwner(address _address) public /*onlyMaster*/ returns (bool);
+  /**
+   * @dev A function that sets a role for a wallet.
+   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole)
+   * @param _address The wallet whose role needs to be set.
+   * @param _role The role to be set.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setRole(address _address, uint8 _role) public /*onlyMasterOrIssuer*/ returns (bool);
+  /**
+   * @dev A function that removes a wallet's role.
+   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole)
+   * @param _address The wallet whose role needs to be set.
+   * @param _role The role to be set.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function removeRole(address _address) public /*onlyMasterOrIssuer*/ returns (bool);
   function getRole(address _address) public view returns (uint8);
 }
