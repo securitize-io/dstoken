00721a5cb4ecc017824f9b0ed83b28fdb1e25ca4
adam.zigdon@gmail.com <adam@securitize.io>
2018-06-13 11:30:53 +0300

    add some more comments

--- contracts/trust/DSTrustService.sol
+++ contracts/trust/DSTrustService.sol
@@ -2,26 +2,48 @@ pragma solidity ^0.4.23;
 
 import "./DSTrustServiceInterface.sol";
 
+/**
+ * @title DSTrustService
+ * @dev A trust service which allows role-based access control for other contracts.
+ * @dev Implements DSTrustServiceInterface with local storage.
+ */
 contract DSTrustService is DSTrustServiceInterface {
+  // holds the role for each user.
   mapping (address => uint8) roles;
 
+  // holds the current MASTER for quick replacement in setOwner.
   address public owner;
 
+  /**
+   * @dev Constructor to set the contract creator as MASTER.
+   */
   constructor() public {
     owner = msg.sender;
     roles[msg.sender] = MASTER;
   }
 
+  /**
+   * @dev Allow invoking of functions only by the user who has the MASTER role.
+   */
   modifier onlyMaster() {
     require(roles[msg.sender] == MASTER);
     _;
   }
 
+  /**
+   * @dev Allow invoking of functions only by the users who have the MASTER role or the ISSUER role.
+   */
   modifier onlyMasterOrIssuer() {
     require(roles[msg.sender] == MASTER || roles[msg.sender] == ISSUER);
     _;
   }
 
+  /**
+   * @dev Sets or removes a role for a wallet. (internal)
+   * @param _address The wallet whose role needs to be set or removed.
+   * @param _role The role to be set. NONE (0) indicates role removal.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setRoleImpl(address _address, uint8 _role) internal returns (bool) {
     uint8 old_role = roles[_address];
 
@@ -38,6 +60,11 @@ contract DSTrustService is DSTrustServiceInterface {
     return true;
   }
 
+  /**
+   * @dev Transfers the ownership (MASTER role) of the contract.
+   * @param _address The address which the ownership needs to be transferred to.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setOwner(address _address) public onlyMaster returns (bool) {
     require(setRoleImpl(owner, NONE));
     owner = _address;
@@ -45,12 +72,25 @@ contract DSTrustService is DSTrustServiceInterface {
     return true;
   }
 
+  /**
+   * @dev Sets a role for a wallet.
+   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole).
+   * @param _address The wallet whose role needs to be set.
+   * @param _role The role to be set.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function setRole(address _address, uint8 _role) public onlyMasterOrIssuer returns (bool) {
     require(_role == ISSUER || _role == EXCHANGE);
 
     return setRoleImpl(_address, _role);
   }
 
+  /**
+   * @dev Removes the role for a wallet.
+   * @dev Should not be used to remove MASTER (use setOwner).
+   * @param _address The wallet whose role needs to be removed.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function removeRole(address _address) public onlyMasterOrIssuer returns (bool) {
     uint8 role = roles[_address];
 
@@ -59,6 +99,11 @@ contract DSTrustService is DSTrustServiceInterface {
     return setRoleImpl(_address, NONE);
   }
 
+  /**
+   * @dev Gets the role for a wallet.
+   * @param _address The wallet whose role needs to be fetched.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function getRole(address _address) public view returns (uint8) {
     return roles[_address];
   }
--- contracts/trust/DSTrustServiceInterface.sol
+++ contracts/trust/DSTrustServiceInterface.sol
@@ -2,22 +2,36 @@ pragma solidity ^0.4.23;
 
 /**
  * @title DSTrustServiceInterface
- * @dev An interface for a trust service which allows role-based access control for other contracts
+ * @dev An interface for a trust service which allows role-based access control for other contracts.
  */
 contract DSTrustServiceInterface {
+  /**
+   * @dev Should be emitted when a role is set for a user.
+   */
   event DSTrustServiceRoleAdded(address _address, uint8 _role, address _sender);
+  /**
+   * @dev Should be emitted when a role is removed for a user.
+   */
   event DSTrustServiceRoleRemoved(address _address, uint8 _role, address _sender);
 
+
+  // Role constants
   uint8 public constant NONE = 0;
   uint8 public constant MASTER = 1;
   uint8 public constant ISSUER = 2;
   uint8 public constant EXCHANGE = 4;
 
+  /**
+   * @dev Allow invoking of functions only by the user who has the MASTER role.
+   */
   modifier onlyMaster() {
     assert(false);
     _;
   }
 
+  /**
+   * @dev Allow invoking of functions only by the users who have the MASTER role or the ISSUER role.
+   */
   modifier onlyMasterOrIssuer() {
     assert(false);
     _;
@@ -37,20 +51,24 @@ contract DSTrustServiceInterface {
    */
   function setOwner(address _address) public /*onlyMaster*/ returns (bool);
   /**
-   * @dev A function that sets a role for a wallet.
-   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole)
+   * @dev Sets a role for a wallet.
+   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole).
    * @param _address The wallet whose role needs to be set.
    * @param _role The role to be set.
    * @return A boolean that indicates if the operation was successful.
    */
   function setRole(address _address, uint8 _role) public /*onlyMasterOrIssuer*/ returns (bool);
   /**
-   * @dev A function that removes a wallet's role.
-   * @dev Should not be used for setting MASTER (use setOwner) or role removal (use removeRole)
-   * @param _address The wallet whose role needs to be set.
-   * @param _role The role to be set.
+   * @dev Removes the role for a wallet.
+   * @dev Should not be used to remove MASTER (use setOwner).
+   * @param _address The wallet whose role needs to be removed.
    * @return A boolean that indicates if the operation was successful.
    */
   function removeRole(address _address) public /*onlyMasterOrIssuer*/ returns (bool);
+  /**
+   * @dev Gets the role for a wallet.
+   * @param _address The wallet whose role needs to be fetched.
+   * @return A boolean that indicates if the operation was successful.
+   */
   function getRole(address _address) public view returns (uint8);
 }
