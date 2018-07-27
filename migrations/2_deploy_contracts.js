9374bc35ccf9afb55a9d2ceb719a526d83fbc481
Adam Zigdon <adam@securitize.io>
2018-07-27 14:41:05 +0300

    add migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,8 @@ const ESComplianceServiceWhitelisted = artifacts.require('ESComplianceServiceWhi
 const ESComplianceServiceNormal = artifacts.require('ESComplianceServiceWhitelisted'); // TODO: change this!
 
 const ESRegistryService = artifacts.require('ESRegistryService');
+const ESWalletManager = artifacts.require('ESWalletManager');
+const ESLockManager = artifacts.require('ESLockManager');
 const DSToken = artifacts.require('DSToken');
 const Proxy = artifacts.require('proxy');
 const argv = require('minimist')(process.argv.slice(2));
@@ -15,6 +17,9 @@ const DS_TOKEN = 2;
 const REGISTRY_SERVICE = 4;
 const COMPLIANCE_SERVICE = 8;
 const COMMS_SERVICE = 16;
+const WALLET_MANAGER=32;
+const LOCK_MANAGER=64;
+const ISSUANCE_INFORMATION_MANAGER=128;
 
 const NONE = 0;
 const MASTER = 1;
@@ -49,6 +54,9 @@ module.exports = function (deployer) {
   let proxy = null;
   let token = null;
   let registry = null;
+  let walletManager = null;
+  let lockManager = null;
+  let issuanceInformationManager = null;
 
   // Deploy eternal storage
   deployer.deploy(EternalStorage).then(s => {
@@ -84,6 +92,12 @@ module.exports = function (deployer) {
   }).then(s => {
     // Deploy token
     complianceService = s;
+    return deployer.deploy(ESWalletManager, storage.address, `${name}WalletManager`);
+  }).then(s => {
+    walletManager = s;
+    return deployer.deploy(ESLockManager, storage.address, `${name}LockManager`);
+  }).then(s => {
+    lockManager = s;
     return deployer.deploy(DSToken);
   }).then(s => {
     tokenImpl = s;
@@ -105,6 +119,12 @@ module.exports = function (deployer) {
     console.log('Adding write right on eternal storage to compliance service');
     return storage.adminAddRole(complianceService.address, 'write');
   }).then(() => {
+    console.log('Adding write right on eternal storage to wallet manager');
+    return storage.adminAddRole(walletManager.address, 'write');
+  }).then(() => {
+    console.log('Adding write right on eternal storage to lock manager');
+    return storage.adminAddRole(lockManager.address, 'write');
+  }).then(() => {
     if (registry) {
       console.log('Adding write right on eternal storage to registry');
       return storage.adminAddRole(registry.address, 'write');
@@ -124,6 +144,12 @@ module.exports = function (deployer) {
       return complianceService.setDSService(REGISTRY_SERVICE, registry.address);
     }
   }).then(() => {
+    console.log('Connecting compliance manager to wallet manager');
+    return complianceService.setDSService(WALLET_MANAGER, walletManager.address);
+  }).then(() => {
+    console.log('Connecting compliance manager to lock manager');
+    return complianceService.setDSService(LOCK_MANAGER, lockManager.address);
+  }).then(() => {
     if (registry) {
       console.log('Connecting registry to trust service');
       return registry.setDSService(TRUST_SERVICE, trustService.address);
@@ -135,14 +161,31 @@ module.exports = function (deployer) {
     console.log('Connecting token to compliance service');
     return token.setDSService(COMPLIANCE_SERVICE, complianceService.address);
   }).then(() => {
+    console.log('Connecting token to wallet manager');
+    return token.setDSService(WALLET_MANAGER, walletManager.address);
+  }).then(() => {
+    console.log('Connecting token to lock manager');
+    return token.setDSService(LOCK_MANAGER, lockManager.address);
+  }).then(() => {
     console.log('Connecting compliance service to token');
     return complianceService.setDSService(DS_TOKEN, token.address);
+  }).then(() => {
+    console.log('Connecting wallet manager to trust service');
+    return walletManager.setDSService(TRUST_SERVICE, trustService.address);
+  }).then(() => {
+    console.log('Connecting lock manager to trust service');
+    return lockManager.setDSService(TRUST_SERVICE, trustService.address);
+  }).then(() => {
+    console.log('Connecting lock manager to token');
+    return lockManager.setDSService(DS_TOKEN, token.address);
   }).then(() => {
     console.log(`\n\nToken ${name} deployment complete`);
     console.log('-------------------------');
     console.log(`Token is at address: ${token.address} (behind proxy)`);
     console.log(`Token implementation is at address: ${tokenImpl.address}`);
     console.log(`Compliance service is at address: ${complianceService.address}, and is of type ${complianceManagerType}.`);
+    console.log(`Wallet manager is at address: ${walletManager.address}`);
+    console.log(`Lock manager is at address: ${lockManager.address}`);
     console.log(`Trust service is at address: ${trustService.address}`);
     console.log(`Eternal storage is at address: ${storage.address}`);
     if (registry) {
