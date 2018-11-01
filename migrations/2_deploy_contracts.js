af030b7e8f877ccd97e8818a4c1560c3df9fe044
Adam Zigdon <adam.zigdon@gmail.com>
2018-11-01 21:57:03 +0200

    update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,7 +11,7 @@ const ESLockManager = artifacts.require('ESLockManager');
 const ESInvestorLockManager = artifacts.require('ESInvestorLockManager');
 const DSToken = artifacts.require('DSToken');
 const Proxy = artifacts.require('Proxy');
-const ESTokenIssuer = artifacts.require('ESMegaTokenIssuer');
+const ESTokenIssuer = artifacts.require('ESTokenIssuer');
 const argv = require('minimist')(process.argv.slice(2));
 
 const TRUST_SERVICE = 1;
