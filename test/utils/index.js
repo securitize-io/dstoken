537676ecf20a665614841761cfe3344b54883943
Omri Perl <omri@securitize.io>
2019-06-04 12:55:24 +0300

    Added multisig wallet contract + tests

--- test/utils/index.js
+++ test/utils/index.js
@@ -10,7 +10,39 @@ async function setServicesDependencies(service, depTypes, depAddresses) {
   }
 }
 
+function getParamFromTxEvent(
+  transaction,
+  paramName,
+  contractFactory,
+  eventName
+) {
+  assert.isObject(transaction);
+  let logs = transaction.logs;
+  if (eventName != null) {
+    logs = logs.filter(l => l.event === eventName);
+  }
+  assert.equal(logs.length, 1, 'too many logs found!');
+  let param = logs[0].args[paramName];
+  if (contractFactory != null) {
+    let contract = contractFactory.at(param);
+    assert.isObject(contract, `getting ${paramName} failed for ${param}`);
+    return contract;
+  } else {
+    return param;
+  }
+}
+
+function balanceOf(web3, account) {
+  return new Promise((resolve, reject) =>
+    web3.eth.getBalance(account, (e, balance) =>
+      e ? reject(e) : resolve(balance)
+    )
+  );
+}
+
 module.exports = {
   addWriteRoles,
   setServicesDependencies,
+  getParamFromTxEvent,
+  balanceOf,
 };
