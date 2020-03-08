047a20e025ee72942292aa91e4828b9b3c93027c
Omri Perl <omri@securitize.io>
2020-03-08 12:14:45 +0200

    compliance tests wip

--- test/integration/OmnibusWalletCompliance.test.js
+++ test/integration/OmnibusWalletCompliance.test.js
@@ -1,4 +1,5 @@
 const assertRevert = require("../utils/assertRevert");
+let latestTime = require("../utils/latestTime");
 const deployContracts = require("../utils").deployContracts;
 const fixtures = require("../fixtures");
 const globals = require("../../utils/globals");
@@ -12,6 +13,7 @@ const attributeStatus = fixtures.AttributeStatus;
 const country = fixtures.Country;
 const compliance = fixtures.Compliance;
 const address = fixtures.Address;
+const time = fixtures.Time;
 
 contract("OmnibusWalletCompliance", function([
   owner,
@@ -72,10 +74,6 @@ contract("OmnibusWalletCompliance", function([
       investorWallet1,
       investorId.GENERAL_INVESTOR_ID_1
     );
-    await this.registryService.setCountry(
-      investorId.GENERAL_INVESTOR_ID_1,
-      country.USA
-    );
   });
 
   describe("Omnibus to omnibus transfer", function() {
@@ -94,19 +92,81 @@ contract("OmnibusWalletCompliance", function([
       assert.equal(res[0].toNumber(), 81);
       assert.equal(res[1], "Omnibus to omnibus transfer");
     });
+  });
 
-    describe("Flow of tokens restrictions", function() {
-      describe("Deposit", function() {
-        it("It should not allow transfer is us lock period has not expired", async function() {});
+  describe("Flow of tokens restrictions", function() {
+    describe("Deposit", function() {
+      it("It should not allow deposit when there are locked tokens", async function() {
+        await this.token.issueTokens(investorWallet1, 1000);
+        await this.lockManager.addManualLockRecord(
+          investorWallet1,
+          100,
+          "Test",
+          (await latestTime()) + 1000
+        );
+        const res = await this.complianceService.preInternalTransferCheck(
+          investorWallet1,
+          omnibusWallet1,
+          1000,
+          address.ZERO_ADDRESS
+        );
+        assert.equal(res[0].toNumber(), 16);
+        assert.equal(res[1], "Tokens locked");
+      });
+
+      it("It should not allow deposit if us lock period has not expired", async function() {
+        await this.complianceConfiguration.setUsLockPeriod(time.YEARS);
+        await this.registryService.setCountry(
+          investorId.GENERAL_INVESTOR_ID_1,
+          country.USA
+        );
+        await this.token.issueTokens(investorWallet1, 1000);
+        const res = await this.complianceService.preInternalTransferCheck(
+          investorWallet1,
+          omnibusWallet1,
+          500,
+          address.ZERO_ADDRESS
+        );
+        assert.equal(res[0].toNumber(), 32);
+        assert.equal(res[1], "Hold-up 1y");
+      });
+
+      it("It should not allow deposit if non us lock period has not expired", async function() {
+        await this.complianceConfiguration.setNonUsLockPeriod(time.YEARS);
+        await this.token.issueTokens(investorWallet1, 1000);
+        const res = await this.complianceService.preInternalTransferCheck(
+          investorWallet1,
+          omnibusWallet1,
+          500,
+          address.ZERO_ADDRESS
+        );
+        assert.equal(res[0].toNumber(), 33);
+        assert.equal(res[1], "Hold-up");
+      });
+
+      it("It should not allow transfer if non us investor deposits tokens to a us omnibus in flowback period", async function() {
+        await this.registryService.setCountry(
+          investorId.OMNIBUS_WALLET_INVESTOR_ID_1,
+          country.USA
+        );
+        await this.token.issueTokens(investorWallet1, 1000);
+        const res = await this.complianceService.preInternalTransferCheck(
+          investorWallet1,
+          omnibusWallet1,
+          500,
+          address.ZERO_ADDRESS
+        );
+        assert.equal(res[0].toNumber(), 25);
+        assert.equal(res[1], "Flowback");
       });
     });
+  });
 
-    describe("Tokens amounts restrictions", function() {
-      it("It should fail the transfer", async function() {});
-    });
+  describe("Tokens amounts restrictions", function() {
+    it("It should fail the transfer", async function() {});
+  });
 
-    describe("Investor restrictions", function() {
-      it("It should fail the transfer", async function() {});
-    });
+  describe("Investor restrictions", function() {
+    it("It should fail the transfer", async function() {});
   });
 });
