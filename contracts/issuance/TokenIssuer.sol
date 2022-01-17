8c94c833c877b340c5fb4893c4c228366c9a4be3
Diego Mernies <diego.mernies@securitize.io>
2022-01-17 15:25:42 -0300

    Fix: improvement to reduce gas consumption

--- contracts/issuance/TokenIssuer.sol
+++ contracts/issuance/TokenIssuer.sol
@@ -8,7 +8,7 @@ contract TokenIssuer is ProxyTarget, Initializable, IDSTokenIssuer, ServiceConsu
     function initialize() public initializer forceInitializeFromProxy {
         IDSTokenIssuer.initialize();
         ServiceConsumer.initialize();
-        VERSIONS.push(4);
+        VERSIONS.push(5);
     }
 
     function issueTokens(
@@ -26,24 +26,24 @@ contract TokenIssuer is ProxyTarget, Initializable, IDSTokenIssuer, ServiceConsu
         require(_issuanceValues.length == 2, "Wrong length of parameters");
         require(_attributeValues.length == _attributeExpirations.length, "Wrong length of parameters");
         require(_locksValues.length == _lockReleaseTimes.length, "Wrong length of parameters");
-
-        if (getRegistryService().isWallet(_to)) {
-            require(CommonUtils.isEqualString(getRegistryService().getInvestor(_to), _id), "Wallet does not belong to investor");
+        IDSRegistryService registryService = getRegistryService();
+        if (registryService.isWallet(_to)) {
+            require(CommonUtils.isEqualString(registryService.getInvestor(_to), _id), "Wallet does not belong to investor");
         } else {
-            if (!getRegistryService().isInvestor(_id)) {
-                getRegistryService().registerInvestor(_id, _collisionHash);
-                getRegistryService().setCountry(_id, _country);
+            if (!registryService.isInvestor(_id)) {
+                registryService.registerInvestor(_id, _collisionHash);
+                registryService.setCountry(_id, _country);
 
                 if (_attributeValues.length > 0) {
                     require(_attributeValues.length == 3, "Wrong length of parameters");
-                    getRegistryService().setAttribute(_id, KYC_APPROVED, _attributeValues[0], _attributeExpirations[0], "");
-                    getRegistryService().setAttribute(_id, ACCREDITED, _attributeValues[1], _attributeExpirations[1], "");
-                    getRegistryService().setAttribute(_id, QUALIFIED, _attributeValues[2], _attributeExpirations[2], "");
+                    registryService.setAttribute(_id, KYC_APPROVED, _attributeValues[0], _attributeExpirations[0], "");
+                    registryService.setAttribute(_id, ACCREDITED, _attributeValues[1], _attributeExpirations[1], "");
+                    registryService.setAttribute(_id, QUALIFIED, _attributeValues[2], _attributeExpirations[2], "");
                 }
 
             }
 
-            getRegistryService().addWallet(_to, _id);
+            registryService.addWallet(_to, _id);
         }
 
         getToken().issueTokensWithMultipleLocks(_to, _issuanceValues[0], _issuanceValues[1], _locksValues, _reason, _lockReleaseTimes);
