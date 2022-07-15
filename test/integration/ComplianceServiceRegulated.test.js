2eaca60e2ae8bcfd75df70f8a5316ef1da781471
David Ezequiel Assen <david.assen@securitize.io>
2022-07-15 19:46:36 -0300

    Added seize test

--- test/integration/ComplianceServiceRegulated.test.js
+++ test/integration/ComplianceServiceRegulated.test.js
@@ -220,7 +220,7 @@ contract("ComplianceServiceRegulated", function([
       await assertRevert(this.token.transfer(owner, 100, {from: wallet}));
     });
 
-    it("Should not decrease total investors value when transfer tokens", async function() {
+    it("Should NOT decrease total investors value when transfer tokens", async function() {
       assert.equal(
         (await this.complianceService.getTotalInvestorsCount()).toNumber(),
         0
@@ -682,6 +682,40 @@ contract("ComplianceServiceRegulated", function([
       await this.token.transfer(omnibusWallet, 50, {from: wallet});
       await assertRevert(
         this.token.seize(omnibusWallet, issuerWallet, 40, "Test")
+      );
+    });
+
+    it("Should NOT decrease total investors value when seizing tokens", async function() {
+      assert.equal(
+        (await this.complianceService.getTotalInvestorsCount()).toNumber(),
+        0
+      );
+      await this.registryService.registerInvestor(
+        investorId.GENERAL_INVESTOR_ID_1,
+        investorId.GENERAL_INVESTOR_COLLISION_HASH_1
+      );
+      await this.registryService.addWallet(
+        wallet,
+        investorId.GENERAL_INVESTOR_ID_1
+      );
+      await this.token.setCap(1000);
+      await this.token.issueTokens(wallet, 100);
+      assert.equal(
+        await this.registryService.getInvestor(wallet),
+        investorId.GENERAL_INVESTOR_ID_1
+      );
+      assert.equal(
+        (await this.complianceService.getTotalInvestorsCount()).toNumber(),
+        1
+      );
+
+      assert.equal(await this.token.balanceOf(wallet), 100);
+      await this.token.seize(wallet, issuerWallet, 100, "Test");
+      assert.equal(await this.token.balanceOf(wallet), 0);
+
+      assert.equal(
+        (await this.complianceService.getTotalInvestorsCount()).toNumber(),
+        1
       );
     });
 
