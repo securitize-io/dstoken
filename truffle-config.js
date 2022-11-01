fe3b989e18da17f57e780cb53d2dc58d5159a5bc
Joaquin Larranaga <joaquin.larranaga@bureapr.com>
2022-11-01 12:14:34 -0300

    remove gas reporter

--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@ const privateKey =
 module.exports = {
   networks: {
     development: {
-      // provider: () => new Web3.providers.HttpProvider('http://127.0.0.1:7545'),
+      provider: () => new Web3.providers.HttpProvider('http://127.0.0.1:7545'),
       host: 'localhost',
       port: 7545,
       network_id: '*',
@@ -93,6 +93,6 @@ module.exports = {
   },
   plugins: [
     'truffle-contract-size',
-    'eth-gas-reporter',
+    // 'eth-gas-reporter',
   ],
 };
