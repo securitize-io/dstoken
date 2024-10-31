fac63e9a9e7104ab3df0618eda789a9da556dece
Fernando Ribas <fribas84@gmail.com>
2024-10-31 14:05:12 -0300

    Changed SPDX-License-Identifier: GPL-3.0 to  SPDX-License-Identifier: Apache-2.0 Adding disclaimer.

--- contracts/compliance/InvestorLockManager.sol
+++ contracts/compliance/InvestorLockManager.sol
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2024 Securitize Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pragma solidity ^0.8.20;
 
 import "./IDSLockManager.sol";
@@ -6,7 +24,6 @@ import "../data-stores/InvestorLockManagerDataStore.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "../utils/BaseDSContract.sol";
 
-//SPDX-License-Identifier: GPL-3.0
 contract InvestorLockManager is InvestorLockManagerBase {
     uint256 constant MAX_LOCKS_PER_INVESTOR = 30;
 
--- contracts/compliance/WalletManager.sol
+++ contracts/compliance/WalletManager.sol
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2024 Securitize Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pragma solidity ^0.8.20;
 
 import "./IDSWalletManager.sol";
@@ -9,7 +27,7 @@ import "../utils/BaseDSContract.sol";
  * @dev A wallet manager which allows marking special wallets in the system.
  * @dev Implements DSTrustServiceInterface and ESServiceConsumer.
  */
-//SPDX-License-Identifier: GPL-3.0
+
 contract WalletManager is IDSWalletManager, WalletManagerDataStore, BaseDSContract {
     function initialize() public override onlyProxy initializer {
         __BaseDSContract_init();
