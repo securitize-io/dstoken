bfdbdd01994334ea0de70b446eedc252dd42fcf5
Fernando Ribas <fribas84@gmail.com>
2024-10-31 14:05:12 -0300

    Changed SPDX-License-Identifier: GPL-3.0 to  SPDX-License-Identifier: Apache-2.0 Adding disclaimer.

--- contracts/token/TokenPartitionsLibrary.sol
+++ contracts/token/TokenPartitionsLibrary.sol
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2024 Securitize Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pragma solidity ^0.8.20;
 
 import "../utils/CommonUtils.sol";
@@ -9,7 +27,6 @@ import "../compliance/IDSPartitionsManager.sol";
 import "../omnibus/IDSOmnibusTBEController.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-//SPDX-License-Identifier: GPL-3.0
 library TokenPartitionsLibrary {
 
     uint256 internal constant COMPLIANCE_SERVICE = 0;
--- contracts/utils/TransactionRelayer.sol
+++ contracts/utils/TransactionRelayer.sol
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2024 Securitize Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pragma solidity ^0.8.20;
 
 import "./BaseDSContract.sol";
@@ -5,7 +23,7 @@ import "./BaseDSContract.sol";
 /**
  @dev Based on SimpleWallet (https://github.com/christianlundkvist/simple-multisig) and uses EIP-712 standard validate a signature
 */
-//SPDX-License-Identifier: GPL-3.0
+
 contract TransactionRelayer is BaseDSContract {
     // EIP712 Precomputed hashes:
     // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)")
