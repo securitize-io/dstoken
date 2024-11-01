c8e0c8c2dbb29e17377882750efbc09d36d8de9f
Fernando Ribas <fribas84@gmail.com>
2024-11-01 10:30:38 -0300

    added task to adhoc deploy libraries

---
+++ tasks/deploy-libraries.ts
@@ -0,0 +1,35 @@
+import { task, types } from 'hardhat/config';
+
+task('deploy-libraries', 'Deploy DS Protocol libraries')
+  .setAction(
+    async (args, hre, run) => {
+      console.log('Deploying libraries');
+      
+      console.log('ComplianceServiceLibrary...');
+      const ComplianceLib = await hre.ethers.getContractFactory(
+        'ComplianceServiceLibrary',
+      );
+      const complianceLib = await ComplianceLib.deploy();
+      console.log(`ComplianceServiceLibrary deployed at: ${complianceLib.target}`);
+
+      console.log('ComplianceServicePartitionedLibrary...');
+      const ComplianceServicePartitionedLibrary = await hre.ethers.getContractFactory(
+        'ComplianceServicePartitionedLibrary',
+      );
+      const complianceServicePartitionedLibrary = await ComplianceServicePartitionedLibrary.deploy();
+      console.log(`ComplianceServiceLibrary deployed at: ${complianceServicePartitionedLibrary.target}`);
+
+      console.log('TokenLibrary...');
+      const TokenLibrary = await hre.ethers.getContractFactory(
+        'TokenLibrary',
+      );
+      const tokenLibrary = await TokenLibrary.deploy();
+      console.log(`ComplianceServiceLibrary deployed at: ${tokenLibrary.target}`);
+
+      console.log('TokenPartitionsLibrary...');
+      const TokenPartitionsLibrary = await hre.ethers.getContractFactory(
+        'TokenPartitionsLibrary',
+      );
+      const tokenPartitionsLibrary = await TokenPartitionsLibrary.deploy();
+      console.log(`ComplianceServiceLibrary deployed at: ${tokenPartitionsLibrary.target}`);
+    });
