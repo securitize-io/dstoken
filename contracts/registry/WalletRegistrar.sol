e49ed25ec3706427c57a3db8976b4975e3e32411
Joaquin Larranaga <joaquin.larranaga@securitize.io>
2025-09-29 12:53:17 -0300

    BC-1476 Audit Issue#18 Asymmetry enforcement between TokenIssuer::registerInvestor and WalletRegistrar::registerWallet

--- contracts/registry/WalletRegistrar.sol
+++ contracts/registry/WalletRegistrar.sol
@@ -35,27 +35,17 @@ contract WalletRegistrar is IDSWalletRegistrar, BaseDSContract {
         uint256[] memory _attributeValues,
         uint256[] memory _attributeExpirations
     ) public override onlyOwnerOrIssuerOrAbove returns (bool) {
-        require(_attributeValues.length == _attributeIds.length, "Wrong length of parameters");
-        require(_attributeIds.length == _attributeExpirations.length, "Wrong length of parameters");
-
         IDSRegistryService registryService = getRegistryService();
 
-        if (!registryService.isInvestor(_id)) {
-            registryService.registerInvestor(_id, _collisionHash);
-            registryService.setCountry(_id, _country);
-        }
-
-        for (uint256 i = 0; i < _wallets.length; i++) {
-            if (registryService.isWallet(_wallets[i])) {
-                require(CommonUtils.isEqualString(registryService.getInvestor(_wallets[i]), _id), "Wallet belongs to a different investor");
-            } else {
-                registryService.addWallet(_wallets[i], _id);
-            }
-        }
-
-        for (uint256 i = 0; i < _attributeIds.length; i++) {
-            registryService.setAttribute(_id, _attributeIds[i], _attributeValues[i], _attributeExpirations[i], "");
-        }
+        registryService.updateInvestor(
+            _id,
+            _collisionHash,
+            _country,
+            _wallets,
+            _attributeIds,
+            _attributeValues,
+            _attributeExpirations
+        );
 
         return true;
     }
