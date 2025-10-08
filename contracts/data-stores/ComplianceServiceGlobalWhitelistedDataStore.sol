998707b9acc860ccd4a5650f5ec748f3e6a1bb6b
Lopez Gonzalo <gonzalo@smtv.dev>
2025-10-08 18:49:43 -0300

    feat: implement ComplianceServiceGlobalWhitelistedDataStore and rename blackList to GlobalWhitelisted

---
+++ contracts/data-stores/ComplianceServiceGlobalWhitelistedDataStore.sol
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2025 Securitize Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pragma solidity 0.8.22;
+
+import {ServiceConsumerDataStore} from "./ServiceConsumerDataStore.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+/**
+ * @title Data store for compliance service blacklist functionality
+ *
+ * This contract stores blacklist-related state variables following the project's
+ * data store pattern for upgradeability and maintainability.
+ */
+contract ComplianceServiceGlobalWhitelistedDataStore is ServiceConsumerDataStore {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    // Blacklist state variables
+    EnumerableSet.AddressSet internal _blacklistedWallets;
+    mapping(address wallet => string reason) internal _blacklistReasons;
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * Calculated based on the complete inheritance hierarchy:
+     * - ServiceConsumerDataStore: 1 slot (services mapping)
+     * - ComplianceServiceDataStore: 9 slots (5 uint256 + 4 mappings)
+     * - Blacklist variables: 2 slots (1 EnumerableSet + 1 mapping)
+     * - Total: 12 slots used, leaving 38 for future expansion
+     */
+    uint256[38] private __gap;
+}
