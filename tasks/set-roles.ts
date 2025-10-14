f630508855e269a175c552bc532417d14e20b35f
Lopez Gonzalo <gonzalo@smtv.dev>
2025-10-14 18:14:43 -0300

    chore: add tx.wait in hardhat tasks

--- tasks/set-roles.ts
+++ tasks/set-roles.ts
@@ -4,16 +4,20 @@ import { DSConstants } from '../utils/globals';
 subtask('set-roles', 'Set roles')
   .addParam('dsContracts', 'Json DS Contract Addresses', undefined, types.json, false)
   .setAction(
-    async (args, hre, run) => {
+    async (args) => {
       const { dsContracts } = args;
 
       console.log(`Granting issuer permissions to Token Issuer ${await dsContracts.tokenIssuer.getAddress()}`);
-      await dsContracts.trustService.setRole(await dsContracts.tokenIssuer.getAddress(), DSConstants.roles.ISSUER);
+      let tx = await dsContracts.trustService.setRole(await dsContracts.tokenIssuer.getAddress(), DSConstants.roles.ISSUER);
+      await tx.wait();
       console.log(`Granting issuer permissions to Wallet Registrar`);
-      await dsContracts.trustService.setRole(await dsContracts.walletRegistrar.getAddress(), DSConstants.roles.ISSUER);
+      tx = await dsContracts.trustService.setRole(await dsContracts.walletRegistrar.getAddress(), DSConstants.roles.ISSUER);
+      await tx.wait();
       console.log(`Granting issuer permissions to Transaction Relayer`);
-      await dsContracts.trustService.setRole(await dsContracts.transactionRelayer.getAddress(), DSConstants.roles.ISSUER);
+      tx = await dsContracts.trustService.setRole(await dsContracts.transactionRelayer.getAddress(), DSConstants.roles.ISSUER);
+      await tx.wait();
       console.log(`Granting issuer permissions to Bulk Operator`);
-      await dsContracts.trustService.setRole(await dsContracts.bulkOperator.getAddress(), DSConstants.roles.ISSUER);
+      tx = await dsContracts.trustService.setRole(await dsContracts.bulkOperator.getAddress(), DSConstants.roles.ISSUER);
+      await tx.wait();
     }
   );
