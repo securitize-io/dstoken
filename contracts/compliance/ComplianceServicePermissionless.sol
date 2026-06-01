92591fc7575b0408e2339c5fb7f019ccd768503f
Nico Pellegrini <nicolas.pellegrini@securitize.io>
2026-06-01 15:55:22 -0300

    fix@BC-2187@Issue4: Fix skip-issuance-record-when-lockperiod-zero

--- contracts/compliance/ComplianceServicePermissionless.sol
+++ contracts/compliance/ComplianceServicePermissionless.sol
@@ -145,6 +145,10 @@ contract ComplianceServicePermissionless is ComplianceService, ComplianceService
             return true;
         }
 
+        if (getComplianceConfigurationService().getNonUSLockPeriod() == 0) {
+            return true;
+        }
+
         _cleanupIssuances(_to);
 
         require(walletIssuancesCounters[_to] < MAX_ISSUANCES_PER_WALLET, "Issuance cap reached");
